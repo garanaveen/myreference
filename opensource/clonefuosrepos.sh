5b1510cd0892d4fbb3bc357fc2fd13687b616721
garanaveen <garanaveen@gmail.com>
2021-04-21 11:10:09 -0500

    List of all open-source repos that were ever accessed

---
+++ opensource/clonefuosrepos.sh
@@ -0,0 +1,7 @@
+#Clone frequently used open-source repositories
+#Git repo for SSL
+git clone git@github.com:openssl/openssl.git
+
+#Change Directory Interactive
+git clone git@github.com:antonioolf/cdi.git
+
